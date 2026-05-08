e18d9a90c5396898599bf200643d7e9fdbfd9809
xiaox <xiaox@DESKTOP-0G71D34>
2026-05-08 09:29:05 +0800

    rename

---
+++ src/SourceGeneration.ActionDispatcher.Queue/Queue/ActionQueueOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace SourceGeneration.ActionDispatcher.Queue;
+
+public class ActionQueueOptions<TAction>
+{
+    public string? QueueName { get; set; }
+    public bool IsPersisted { get; set; } = true;
+    public int MaxConcurrency { get; set; }
+    public Func<TAction, object> KeySelector { get; set; } = null!;
+}
---
+++ src/SourceGeneration.ActionDispatcher.Queue/Queue/ActionTask.cs
@@ -0,0 +1,10 @@
+﻿namespace SourceGeneration.ActionDispatcher.Queue;
+
+public class ActionTask<TAction> where TAction : notnull
+{
+    public object Key { get; init; } = null!;
+    public string? Queue { get; init; }
+    public TAction Action { get; init; } = default!;
+    public long ScheduledMs { get; init; }
+    public long CreatedMs { get; init; }
+}
